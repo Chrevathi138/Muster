52e03e2c9e7aa4cf54c6fb7fdae1724b94a86602
Revathi-co <mydocapp456@gmail.com>
2026-02-14 17:24:56 +0530

    adding intial project

---
+++ src/main/java/com/skmle/muster/MusterApplication.java
@@ -0,0 +1,13 @@
+package com.skmle.muster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skmle/muster/MusterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skmle.muster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
